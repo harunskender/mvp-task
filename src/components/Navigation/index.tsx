f2b5b13198df155c9057e5bd4d3302c9509da895
Harun Skender <harun.skender@hotmail.com>
2021-11-11 00:03:35 +0100

    finished navigation and added css normalizer in index.scss

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,16 +1,20 @@
+import { ReactComponent as CompanyLogo } from 'assets/icons/logo.svg'
+import { ReactComponent as HamburgerIcon } from 'assets/icons/hamburger-icon.svg'
 import classes from './styles.module.scss'
 
 export default function Navigation() {
   return (
     <div className={classes.navigationContainer}>
-      <div>
-        <div>Logo</div>
-        <div>Hamuberger Icon</div>
+      <div className={classes.firstPartOfNavigation}>
+        <CompanyLogo />
+        <HamburgerIcon />
       </div>
 
-      <div>
-        <div>User photo</div>
-        <div>User name</div>
+      <div className={classes.secondPartOfNavigation}>
+        <div className={classes.nameInitials}>
+          <div>JD</div>
+        </div>
+        <div className={classes.name}>John Doe</div>
       </div>
     </div>
   )
